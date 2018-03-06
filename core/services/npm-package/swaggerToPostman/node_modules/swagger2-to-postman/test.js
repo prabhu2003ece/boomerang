ce3ca15a277e18f9479c855c41eaba63bbfb49c2
Jaspal <jgill@illumina.com>
2018-03-05 17:08:18 -0800

    clean up

--- core/services/npm-package/swaggerToPostman/node_modules/swagger2-to-postman/test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-	sc = require('./convert.js');
-
-var file  = fs.readFileSync('test/data/swagger-with-path.json', 'utf8');
-var scr = new sc();
-var jsonObj = JSON.parse(file);
-var pc = scr.convert(jsonObj);
-
-fs.writeFileSync("collection.json", JSON.stringify(pc,null, 2));
